702b55ac4ed4b02f88617390ed68b952e8233dc2
tphuc012 <tphuc012@tphuc012s-MacBook-Pro.local>
2018-12-06 13:27:11 +0700

    add paginationContainerStyle and paginationDotsStyle to props

--- AppIntroSlider.js
+++ AppIntroSlider.js
@@ -168,8 +168,8 @@ export default class AppIntroSlider extends React.Component {
       : this._renderNextButton();
 
     return (
-      <View style={styles.paginationContainer}>
-        <View style={styles.paginationDots}>
+      <View style={[styles.paginationContainer, this.props.paginationContainerStyle]}>
+        <View style={[styles.paginationDots, this.props.paginationDotsStyle]}>
           {!this.props.bottomButton && skipBtn}
           {this.props.slides.length > 1 &&
             this.props.slides.map((_, i) => (
